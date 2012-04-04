65f0f16c5d265758c8e4f1b8d287048f345db5b3
Muhammad Usman <musman@billfloat.com>
2012-04-04 14:23:32 -0700

    edited readme

---
+++ readme.markdown
@@ -0,0 +1,5 @@
+Simple hack to shorten github.com links to git.io links.
+
+Needs more work. To use:
+
+`./shorten.sh <github.com url>`
--- shorten.sh
+++ shorten.sh
@@ -1,4 +1,4 @@
 curl -i http://git.io -F url=$1 -s -o "shortened.txt"
-ruby ~/Development/runner/split.rb | pbcopy
-ruby ~/Development/runner/split.rb
+ruby ~/shortener/runner/split.rb | pbcopy
+ruby ~/shortener/split.rb
 echo "Copied to clipboard/pasteboard"
