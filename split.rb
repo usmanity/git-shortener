b65b04be7c7242a50b74712157a74ff776367bf8
Muhammad Usman <musman@billfloat.com>
2012-04-04 15:24:12 -0700

    surl added for custom option

--- split.rb
+++ split.rb
@@ -1,4 +1,4 @@
-file = File.read("./shortened.txt")
+file = File.read("shortened.txt")
 array_file = file.to_a
 shorter = array_file[9].split(" ")
 puts shorter[1]
--- surl.sh
+++ surl.sh
@@ -1,5 +1,5 @@
 cd ~/git-shortener
-curl -i http://git.io -F url=$1 -s -o "shortened.txt"
+curl -i http://git.io -F url=$1 -F code=$2 -s -o "shortened.txt"
 ruby ~/git-shortener/split.rb | pbcopy
 ruby ~/git-shortener/split.rb
 echo "Copied to clipboard/pasteboard"
