e48a1a8d7d39f70671c7041ca58bfded73a06119
Muhammad Usman <musman@billfloat.com>
2012-04-04 14:53:40 -0700

    removed dot from path

--- shorten.sh
+++ shorten.sh
@@ -1,4 +1,4 @@
-curl -i http://git.io -F url=$1 -s -o "~/.git-shortened/shortened.txt"
-ruby ~/.git-shortener/split.rb | pbcopy
-ruby ~/.git-shortener/split.rb
+curl -i http://git.io -F url=$1 -s -o "~/git-shortened/shortened.txt"
+ruby ~/git-shortener/split.rb | pbcopy
+ruby ~/git-shortener/split.rb
 echo "Copied to clipboard/pasteboard"
