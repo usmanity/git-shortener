88bd63070f532e0dfa4092d718959db7bd07c52b
Muhammad Usman <muhammad@usmanity.com>
2012-04-04 22:08:32 -0700

    dash added

--- surl.sh
+++ surl.sh
@@ -3,4 +3,4 @@ curl -i http://git.io -F url=$1 -F code=$2 -s -o "shortened.txt"
 ruby ~/git-shortener/split.rb | pbcopy
 ruby ~/git-shortener/split.rb
 echo "Copied to clipboard/pasteboard"
-cd
+cd -
